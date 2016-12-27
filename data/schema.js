eaa07ec4c29ee01c42a84e3a3146b01eb5af54a6
Shahzad T <shahzad1@Shahzads-MacBook-Pro.local>
2016-12-27 16:49:26 -0500

    First commit

---
+++ data/schema.js
@@ -0,0 +1,216 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import {
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+
+import {
+  connectionArgs,
+  connectionDefinitions,
+  connectionFromArray,
+  fromGlobalId,
+  globalIdField,
+  mutationWithClientMutationId,
+  nodeDefinitions,
+} from 'graphql-relay';
+
+import {
+  // Import methods that your schema can use to interact with your database
+  User,
+  Widget,
+  getUser,
+  getViewer,
+  getWidget,
+  getWidgets,
+} from './database';
+
+/**
+ * We get the node interface and field from the Relay library.
+ *
+ * The first method defines the way we resolve an ID to its object.
+ * The second defines the way we resolve an object to its GraphQL type.
+ */
+var {nodeInterface, nodeField} = nodeDefinitions(
+  (globalId) => {
+    var {type, id} = fromGlobalId(globalId);
+    if (type === 'User') {
+      return getUser(id);
+    } else if (type === 'Widget') {
+      return getWidget(id);
+    } else {
+      return null;
+    }
+  },
+  (obj) => {
+    if (obj instanceof User) {
+      return userType;
+    } else if (obj instanceof Widget)  {
+      return widgetType;
+    } else if (obj instanceof Hobby)  {
+      return hobbyType;
+    } else {
+      return null;
+    }
+  }
+);
+
+/**
+ * Define your own types here
+ */
+var faction = {
+    hobbies: [
+      { title:"Cricket" },
+      { title: "Reading" },
+      { title: "Traveling" }
+    ]
+}
+
+/**
+*  Types
+*/
+
+var widgetType = new GraphQLObjectType({
+  name: 'Widget',
+  description: 'A shiny widget',
+  fields: () => ({
+    id: globalIdField('Widget'),
+    name: {
+      type: GraphQLString,
+      description: 'The name of the widget',
+    },
+  }),
+  interfaces: [nodeInterface],
+});
+
+var hobbyType = new GraphQLObjectType({
+  name: 'Hobbies',
+  description: 'A user hobbies',
+  fields: () => ({
+    id: globalIdField('Widget'),
+    title: {
+      type: GraphQLString,
+      description: 'Hobby Type',
+    },
+  }),
+  interfaces: [nodeInterface],
+});
+
+
+/**
+ * Define your own connection types here
+ */
+
+var {connectionType: widgetConnection} =
+  connectionDefinitions({name: 'Widget', nodeType: widgetType});
+
+var {connectionType: hobbyConnection} =
+  connectionDefinitions({name: 'Hobby', nodeType: hobbyType});
+
+
+var userType = new GraphQLObjectType({
+  name: 'User',
+  description: 'A person who uses our app',
+  fields: () => ({
+    id: globalIdField('User'),
+    widgets: {
+      type: widgetConnection,
+      description: 'A person\'s collection of widgets',
+      args: connectionArgs,
+      resolve: (_, args) => connectionFromArray(getWidgets(), args),
+    }
+    hobbies: {
+        type: hobbyConnection,
+        description: 'A person\'s hobbies',
+        args: {
+          status: {
+            type: GraphQLString,
+            defaultValue: 'any',
+          },
+          ...connectionArgs,
+        },
+        resolve: (obj,
+            {status, ...args},
+            context,
+            {rootValue: objectManager}
+          ) => {
+              //console.log( "Root", obj, "Status", status, "Args", args, "Context", context);
+              return connectionFromArray(
+                  faction.hobbies.map((hobby) => hobby),
+                  args
+              )
+            },
+      }
+  }),
+  interfaces: [nodeInterface],
+});
+
+
+/**
+ * This is the type that will be the root of our query,
+ * and the entry point into our schema.
+ */
+var queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    node: nodeField,
+    // Add your own root fields here
+    viewer: {
+      type: userType,
+      resolve: () => getViewer(),
+    },
+  }),
+});
+
+function addHobby(values){
+    var hobbyId = faction.hobbies.push(values)  - 1;
+    return { hobbyId };
+}
+const hobbyAddMutation = mutationWithClientMutationId({
+  name: 'InsertHobby',
+  inputFields: {
+    title: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+  },
+  outputFields: {
+    hobby: {
+      type: hobbyType,
+      resolve: payload => faction.hobbies[payload.hobbyId],
+    }
+  },
+  mutateAndGetPayload: (args) => {
+    return addHobby(args);
+  }
+});
+
+var mutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: () => ({
+    insertHobby: hobbyAddMutation,
+  })
+});
+
+/**
+ * Finally, we construct our schema (whose starting query type is the query
+ * type we defined above) and export it.
+ */
+export var Schema = new GraphQLSchema({
+  query: queryType,
+  // Uncomment the following after adding some mutation fields:
+  mutation: mutationType
+});
